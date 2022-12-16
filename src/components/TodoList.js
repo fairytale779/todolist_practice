729d411aeb2dc38789f6d5e37772f6f21c4b77f7
Donghwa <hhhhong44@gmail.com>
2022-12-16 16:22:27 +0900

    1

---
+++ src/components/TodoList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import TodoItem from "./TodoItem";
+import { useTodoState } from '../TodoContext';
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
+
+function TodoList() {
+    const todos = useTodoState()
+
+
+  return (
+    // <TodoListBlock>
+    //     <TodoItem text="프로젝트 생성하기" done={true}/>
+    //     <TodoItem text="컴포넌트 스타일링하기" done={true}/>
+    //     <TodoItem text="Context 만들기" done={true}/>
+    //     <TodoItem text="기능 구현하기" done={true}/>
+    // </TodoListBlock> 
+    <TodoListBlock>
+        {todos.map(todo => (
+            <TodoItem
+                key={todo.id}
+                id={todo.id}
+                text={todo.text}
+                done={todo.done}
+            />
+        ))}
+    </TodoListBlock>
+  );
+}
+
+export default TodoList;
